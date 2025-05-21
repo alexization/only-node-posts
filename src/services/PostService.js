599b1872c59d27b2e431cc5b44597ac172900e8c
hyoseok <hyoseok@hyoseokui-MacBookPro.local>
2025-05-21 23:58:07 +0900

    feat: 글 생성 서비스 코드 작성

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -7,4 +7,22 @@ class PostService {
     constructor(postRepository) {
         this.postRepository = postRepository;
     }
+
+    async createPost(postData) {
+        const { title, content, author } = postData;
+
+        if (!title || !content || !author) {
+            throw new AppError("제목, 글 내용, 작성자는 필수입니다 !", 400);
+        }
+
+        const newPost = new Post(null, title, content, author);
+        const createdPost = await db.transaction(async (conn) => {
+            const created = await this.postRepository.create(newPost);
+            return created;
+        });
+
+        return createdPost;
+    }
 }
+
+module.exports = new PostService(PostRepository);
